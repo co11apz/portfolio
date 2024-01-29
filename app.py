ecd8227d1eab4e9133d9b27e82e1150d96bd2cf5
roman <romanmalajev@gmail.com>
2024-01-29 10:12:24 +0300

    fix

--- app.py
+++ app.py
@@ -21,18 +21,15 @@ def run_allure():
 
 
 @app.route("/run")
-def run():
-    """ Эта функция запуская и отвечает за тесты страницы /example. """
-
-    cmd = ["./site/assets/autotests.sh"]
-    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE,
-                          stdin=subprocess.PIPE,
-                          universal_newlines=True) as result:
-        out = result.communicate()
-    return render_template('index.html', text=out, json=out)
+def runo():
+    bash_path = "D:/Git/bin/bash.exe"
+    script_path = "C:/Users/roman/PycharmProjects/mysite/site/assets/autotests.sh"
+    cmd = [script_path, bash_path]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True) as result:
+        out, err = result.communicate()
+        print("Output from autotests.sh:")
+    return render_template('index.html', text=out, error=err)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
--- site/assets/autotests.sh
+++ site/assets/autotests.sh
@@ -4,4 +4,5 @@ cd ../../
 pwd
 cd autotests/tests
 pwd
+echo "Hello from autotests.sh"
 pytest -s -v test_main_page.py
